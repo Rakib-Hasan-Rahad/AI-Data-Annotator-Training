8905aebaa4cc3beca1e2a7e0a578ca1c66fd42f5
Rakib-Hasan-Rahad <rahadrakib4@gmail.com>
2025-08-27 15:51:53 -0500

    update 1.0

--- lab01-exercise01.py
+++ lab01-exercise01.py
@@ -1,1 +1,9 @@
+'''
+Author: Rakib
+KUID: 322
+Date: Date file was created
+Lab: lab01
+Last modified: Date file was most recently modified
+Purpose: Description of what this file does
+'''
 print("Welcome to lab 1")
